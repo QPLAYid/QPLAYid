153904a62464ae2305bfca7aca51e39e73472277
Qplaysuper <102419809+QPLAYid@users.noreply.github.com>
2023-01-30 14:10:18 +0900

    Update Playme.sh

--- Playme.sh
+++ Playme.sh
@@ -1,194 +1,209 @@
-#! /bin/bash
+#!/bin/bash
+#
+# Script For Building Android arm64 Kernel
+#
+# Copyright (C) 2021-2023 RooGhz720 <rooghz720@gmail.com>
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
 
- # Script For Building Android arm64 Kernel
- #
- # Copyright (c) 2018-2020 Panchajanya1999 <rsk52959@gmail.com>
- #
- # Licensed under the Apache License, Version 2.0 (the "License");
- # you may not use this file except in compliance with the License.
- # You may obtain a copy of the License at
- #
- #      http://www.apache.org/licenses/LICENSE-2.0
- #
- # Unless required by applicable law or agreed to in writing, software
- # distributed under the License is distributed on an "AS IS" BASIS,
- # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- # See the License for the specific language governing permissions and
- # limitations under the License.
- #
+# Setup colour for the script
+yellow='\033[0;33m'
+white='\033[0m'
+red='\033[0;31m'
+green='\e[0;32m'
 
-#Kernel building script
+# Deleting out "kernel complied" and zip "anykernel" from an old compilation
+echo -e "$green << cleanup >> \n $white"
 
-# Function to show an informational message
-msg() {
-    echo -e "\e[1;32m$*\e[0m"
+rm -rf out
+rm -rf zip
+rm -rf error.log
+
+echo -e "$green << setup dirs >> \n $white"
+
+# With that setup , the script will set dirs and few important thinks
+
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+# MIUI = High Dimens
+# OSS = Low Dimens
+
+export CHATID API_BOT TYPE_KERNEL
+
+
+# Kernel build config
+TYPE="MIUI"
+DEVICE="Redmi note 10 pro"
+KERNEL_NAME="SWEET"
+DEFCONFIG="sweet_defconfig"
+AnyKernel="https://github.com/QPLAYid/Anykernel3"
+AnyKernelbranch="master"
+HOSST="Aku_bot"
+USEER="Papimonyeyo"
+ID="Qplaysuper"
+MESIN="Git Workflows"
+
+
+# setup telegram env
+export WAKTU=$(date +"%T")
+export TGL=$(date +"%d-%m-%Y")
+export BOT_MSG_URL="https://api.telegram.org/bot$API_BOT/sendMessage"
+export BOT_BUILD_URL="https://api.telegram.org/bot$API_BOT/sendDocument"
+
+
+tg_sticker() {
+   curl -s -X POST "https://api.telegram.org/bot$API_BOT/sendSticker" \
+        -d sticker="$1" \
+        -d chat_id=$CHATID
 }
-
-err() {
-    echo -e "\e[1;41m$*\e[0m"
-    exit 1
-}
-
-##------------------------------------------------------##
-##----------Basic Informations, COMPULSORY--------------##
-
-# The defult directory where the kernel should be placed
-KERNEL_DIR=$PWD
-
-# The name of the Kernel, to name the ZIP
-ZIPNAME="SiLonT-TEST"
-
-# The name of the device for which the kernel is built
-MODEL="Redmi 4x"
-
-# The codename of the device
-DEVICE="santoni"
-
-# The defconfig which should be used. Get it from config.gz from
-# your device or check source
-DEFCONFIG=santoni_treble_defconfig
-
-# Specify compiler.
-# 'clang' or 'gcc'
-COMPILER=gcc
-
-# Push ZIP to Telegram. 1 is YES | 0 is NO(default)
-PTTG=1
-	if [ $PTTG = 1 ]
-	then
-		# Set Telegram Chat ID
-		CHATID="5653007871"
-	fi
-
-##------------------------------------------------------##
-##---------Do Not Touch Anything Beyond This------------##
-
-## Set defaults first
-CI_BRANCH=$(git rev-parse --abbrev-ref HEAD)
-token=$TELEGRAM_TOKEN
-export KBUILD_BUILD_HOST CI_BRANCH
-
-## Export CI Env
-export KBUILD_BUILD_VERSION=$DRONE_BUILD_NUMBER
-export CI_BRANCH=$DRONE_BRANCH
-
-#Check Kernel Version
-KERVER=$(make kernelversion)
-
-
-# Set a commit head
-COMMIT_HEAD=$(git log --oneline -1)
-
-#Now Its time for other stuffs like cloning, exporting, etc
-
- clone() {
-	echo " "
-		msg "|| Cloning GCC ||"
-		git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 -b gcc-new gcc64
-		git clone --depth=1 https://github.com/mvaisakh/gcc-arm -b gcc-new gcc32
-		GCC64_DIR=$KERNEL_DIR/gcc64
-		GCC32_DIR=$KERNEL_DIR/gcc32
-
-	msg "|| Cloning Anykernel ||"
-	git clone --depth 1 --no-single-branch https://github.com/Reinazhard/AnyKernel3.git -b sadtoni
-}
-
-##------------------------------------------------------##
-
-exports() {
-	export KBUILD_BUILD_USER="reina"
-	export KBUILD_BUILD_HOST="Laptop-Sangar"
-	export ARCH=arm64
-	export SUBARCH=arm64
-
-	KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-elf-gcc --version | head -n 1)
-	PATH=$GCC64_DIR/bin/:$GCC32_DIR/bin/:/usr/bin:$PATH
-
-	export CROSS_COMPILE_ARM32=$GCC32_DIR/bin/arm-eabi-
-	export CROSS_COMPILE=$GCC64_DIR/bin/aarch64-elf-
-	export PATH KBUILD_COMPILER_STRING
-	export BOT_MSG_URL="https://api.telegram.org/bot$token/sendMessage"
-	export BOT_BUILD_URL="https://api.telegram.org/bot$token/sendDocument"
-	PROCS=$(nproc --all)
-	export PROCS
-}
-
-##---------------------------------------------------------##
 
 tg_post_msg() {
-	curl -s -X POST "$BOT_MSG_URL" -d chat_id="5653007871" \
-	-d "disable_web_page_preview=true" \
-	-d "parse_mode=html" \
-	-d text="$1"
-
+        curl -s -X POST "$BOT_MSG_URL" -d chat_id="$2" \
+        -d "parse_mode=markdown" \
+        -d text="$1"
 }
-
-##----------------------------------------------------------------##
 
 tg_post_build() {
-	#Post MD5Checksum alongwith for easeness
-	MD5CHECK=$(md5sum "$1" | cut -d' ' -f1)
+        #Post MD5Checksum alongwith for easeness
+        MD5CHECK=$(md5sum "$1" | cut -d' ' -f1)
 
-	#Show the Checksum alongwith caption
-	curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
-	-F chat_id="$2"  \
-	-F "disable_web_page_preview=true" \
-	-F "parse_mode=html" \
-	-F caption="$3 | <code>Build Number : </code><b>$DRONE_BUILD_NUMBER</b>"
+        #Show the Checksum alongwith caption
+        curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
+        -F chat_id="$2" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=markdown" \
+        -F caption="$3 MD5 \`$MD5CHECK\`"
 }
 
-##----------------------------------------------------------##
+tg_error() {
+        curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
+        -F chat_id="$2" \
+        -F "disable_web_page_preview=true" \
+        -F "parse_mode=html" \
+        -F caption="$3Failed to build , check <code>error.log</code>"
+}
+
+# clang stuff
+		echo -e "$green << cloning clang >> \n $white"
+		git clone --depth=1 -b release/15.x https://gitlab.com/GhostMaster69-dev/cosmic-clang.git "$HOME"/clang
+
+	export PATH="$HOME/clang/bin:$PATH"
+	export KBUILD_COMPILER_STRING=$("$HOME"/clang/bin/clang --version | head -n 1 | sed -e 's/  */ /g' -e 's/[[:space:]]*$//' -e 's/^.*clang/clang/')
+
+# Setup build process
 
 build_kernel() {
-	if [ "$PTTG" = 1 ]
- 	then
-		tg_post_msg "<b>🔨 $KBUILD_BUILD_VERSION CI Build Triggered</b>%0A<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Date : </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0a<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>Top Commit : </b><a href='$DRONE_COMMIT_LINK'>$COMMIT_HEAD</a>" "$CHATID"
-	fi
+Start=$(date +"%s")
 
-	msg "|| Started Compilation ||"
-	BUILD_START=$(date +"%s")
-	make O=out $DEFCONFIG
-	make -j"$PROCS" O=out ARCH=arm64 LD=ld.lld
+	make -j$(nproc --all) O=out \
+                              ARCH=arm64 \
+                              LLVM=1 \
+                              LLVM_IAS=1 \
+                              AR=llvm-ar \
+                              NM=llvm-nm \
+                              LD=ld.lld \
+                              OBJCOPY=llvm-objcopy \
+                              OBJDUMP=llvm-objdump \
+                              STRIP=llvm-strip \
+                              CC=clang \
+                              CROSS_COMPILE=aarch64-linux-gnu- \
+                              CROSS_COMPILE_ARM32=arm-linux-gnueabi-  2>&1 | tee error.log
 
-		BUILD_END=$(date +"%s")
-		DIFF=$((BUILD_END - BUILD_START))
-
-		if [ -f "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb ]
-	    then
-	    	msg "|| Kernel successfully compiled ||"
-				gen_zip
-		else
-			if [ "$PTTG" = 1 ]
- 			then
-				tg_post_msg "<b>❌ Build failed to compile after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</b>" "$CHATID"
-			fi
-		fi
-
+End=$(date +"%s")
+Diff=$(($End - $Start))
 }
 
-##--------------------------------------------------------------##
+# Let's start
+echo -e "$green << doing pre-compilation process >> \n $white"
+export ARCH=arm64
+export SUBARCH=arm64
+export HEADER_ARCH=arm64
 
-gen_zip() {
-	msg "|| Zipping into a flashable zip ||"
-	mv "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb AnyKernel3/Image.gz-dtb
-	if [ $BUILD_DTBO = 1 ]
-	then
-		mv "$KERNEL_DIR"/out/arch/arm64/boot/dtbo.img AnyKernel3/dtbo.img
-	fi
-	cd AnyKernel3 || exit
-	zip -r9 $ZIPNAME-$DEVICE-"$DRONE_BUILD_NUMBER" * -x .git README.md
+export KBUILD_BUILD_HOST="$HOSST"
+export KBUILD_BUILD_USER="$USEER"
+export KBUILD_BUILD_VERSION="$ID"
 
-	## Prepare a final zip variable
-	ZIP_FINAL="$ZIPNAME-$DEVICE-$DRONE_BUILD_NUMBER.zip"
-	if [ "$PTTG" = 1 ]
- 	then
-		tg_post_build "$ZIP_FINAL" "$CHATID" "✅ Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
-	fi
-	cd ..
-}
+mkdir -p out
 
-clone
-exports
-build_kernel
+make O=out clean && make O=out mrproper
+make "$DEFCONFIG" O=out
 
-##----------------*****-----------------------------##
+echo -e "$yellow << compiling the kernel >> \n $white"
+
+# stiker post
+
+
+build_kernel || error=true
+
+DATE=$(date +"%Y%m%d-%H%M%S")
+KERVER=$(make kernelversion)
+KOMIT=$(git log --pretty=format:'"%h : %s"' -1)
+BRANCH=$(git rev-parse --abbrev-ref HEAD)
+
+export IMG="$MY_DIR"/out/arch/arm64/boot/Image.gz-dtb
+export dtbo="$MY_DIR"/out/arch/arm64/boot/dtbo.img
+export dtb="$MY_DIR"/out/arch/arm64/boot/dtb.img
+
+
+        if [ -f "$IMG" ]; then
+                echo -e "$green << selesai dalam $(($Diff / 60)) menit and $(($Diff % 60)) detik >> \n $white"
+        else
+                echo -e "$red << Gagal dalam membangun kernel!!! , cek kembali kode anda >>$white"
+                tg_post_msg "GAGAL!!! uploading log"
+                tg_error "error.log" "$CHATID"
+                tg_post_msg "done" "$CHATID"
+                rm -rf out
+                rm -rf testing.log
+                rm -rf error.log
+                exit 1
+        fi
+
+TEXT1="
+*Build Completed Successfully*
+━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
+* Device* : \`$DEVICE\`
+* Code name* : \`Sweet | Sweetin\`
+* Variant Build* : \`$TYPE\`
+* Time Build* : \`$(($Diff / 60)) menit\`
+* Branch Build* : \`$BRANCH\`
+* System Build* : \`$MESIN\`
+* Date Build* : \`$TGL\` \`$WAKTU\`
+* Last Commit* : \`$KOMIT\`
+* Author* : @RooGhz720
+━━━━━━━━━ஜ۩۞۩ஜ━━━━━━━━
+"
+
+        if [ -f "$IMG" ]; then
+                echo -e "$green << cloning AnyKernel from your repo >> \n $white"
+                git clone --depth=1 "$AnyKernel" --single-branch -b "$AnyKernelbranch" zip
+                echo -e "$yellow << making kernel zip >> \n $white"
+                cp -r "$IMG" zip/
+                cp -r "$dtbo" zip/
+                cp -r "$dtb" zip/
+                cd zip
+                export ZIP="$KERNEL_NAME"-"$TYPE"-"$TGL"
+                zip -r9 "$ZIP" * -x .git README.md LICENSE *placeholder
+                curl -sLo zipsigner-3.0.jar https://github.com/Magisk-Modules-Repo/zipsigner/raw/master/bin/zipsigner-3.0-dexed.jar
+                java -jar zipsigner-3.0.jar "$ZIP".zip "$ZIP"-signed.zip
+                tg_sticker "CAACAgUAAxkBAAGLlS1jnv1FJAsPoU7-iyZf75TIIbD0MQACYQIAAvlQCFTxT3DFijW-FSwE"
+                tg_post_msg "$TEXT1" "$CHATID"
+                tg_post_build "$ZIP"-signed.zip "$CHATID"
+                cd ..
+                rm -rf error.log
+                rm -rf out
+                rm -rf zip
+                rm -rf testing.log
+                exit
+        fi
