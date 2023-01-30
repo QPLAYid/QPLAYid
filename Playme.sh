fe2613bc62d2b0a96b9221fe22649b70486f4678
QPLAYid <102419809+QPLAYid@users.noreply.github.com>
2023-01-30 13:39:28 +0900

    Update Playme.sh

--- Playme.sh
+++ Playme.sh
@@ -1,69 +1,122 @@
-#! /bin/sh
-set -e o pipefail
+#! /bin/bash
 
-KERNEL_DIR=$PWD
-ARG1=$1 #It is the devicename [generally codename]
-ARG2=$2 #It is the make arguments, whether clean / dirty / def_regs [regenerates defconfig]
-ARG3=$3 #Build should be pushed or not [PUSH / NOPUSH]
-DATE=$(TZ=Asia/Jakarta date +"%Y%m%d-%T")
-export ZIPNAME="DarkOne-chef-Oreo-Pie-" #Specifies the name of kernel
-##---------------------------------------------------##
+ # Script For Building Android arm64 Kernel
+ #
+ # Copyright (c) 2018-2020 Panchajanya1999 <rsk52959@gmail.com>
+ #
+ # Licensed under the Apache License, Version 2.0 (the "License");
+ # you may not use this file except in compliance with the License.
+ # You may obtain a copy of the License at
+ #
+ #      http://www.apache.org/licenses/LICENSE-2.0
+ #
+ # Unless required by applicable law or agreed to in writing, software
+ # distributed under the License is distributed on an "AS IS" BASIS,
+ # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ # See the License for the specific language governing permissions and
+ # limitations under the License.
+ #
 
-#START : Argument 3 [ARG3] Check
-case "$ARG3" in
-  "PUSH" ) # Push build to TG Channel
-      build_push=true
-  ;;
-  "NOPUSH" ) # Do not push
-      build_push=false
-  ;;
-  * ) echo -e "\nError..!! Unknown command. Please refer README.\n"
-      return
-  ;;
-esac # END : Argument 3 [ARG3] Check
+#Kernel building script
 
-##-----------------------------------------------------##
+# Function to show an informational message
+msg() {
+    echo -e "\e[1;32m$*\e[0m"
+}
 
-else
-  echo -e "\nToo many Arguments..!! Provided - $# , Required - 3\nCheck README"
-  return
-#Get outta
-fi
+err() {
+    echo -e "\e[1;41m$*\e[0m"
+    exit 1
+}
 
 ##------------------------------------------------------##
+##----------Basic Informations, COMPULSORY--------------##
+
+# The defult directory where the kernel should be placed
+KERNEL_DIR=$PWD
+
+# The name of the Kernel, to name the ZIP
+ZIPNAME="SiLonT-TEST"
+
+# The name of the device for which the kernel is built
+MODEL="Redmi 4x"
+
+# The codename of the device
+DEVICE="santoni"
+
+# The defconfig which should be used. Get it from config.gz from
+# your device or check source
+DEFCONFIG=santoni_treble_defconfig
+
+# Specify compiler.
+# 'clang' or 'gcc'
+COMPILER=gcc
+
+# Push ZIP to Telegram. 1 is YES | 0 is NO(default)
+PTTG=1
+	if [ $PTTG = 1 ]
+	then
+		# Set Telegram Chat ID
+		CHATID="5653007871"
+	fi
+
+##------------------------------------------------------##
+##---------Do Not Touch Anything Beyond This------------##
+
+## Set defaults first
+CI_BRANCH=$(git rev-parse --abbrev-ref HEAD)
+token=$TELEGRAM_TOKEN
+export KBUILD_BUILD_HOST CI_BRANCH
+
+## Export CI Env
+export KBUILD_BUILD_VERSION=$DRONE_BUILD_NUMBER
+export CI_BRANCH=$DRONE_BRANCH
+
+#Check Kernel Version
+KERVER=$(make kernelversion)
+
+
+# Set a commit head
+COMMIT_HEAD=$(git log --oneline -1)
 
 #Now Its time for other stuffs like cloning, exporting, etc
 
-clone() {
+ clone() {
 	echo " "
-	echo "★★Cloning GCC Toolchain from GitHub .."
-	git clone --progress -j32 --depth 1 --no-single-branch https://github.com/archie9211/linaro -b master linaro
-	git clone --progress -j32 --depth 1 --no-single-branch https://github.com/archie9211/linaro -b arm32 linaro32
+		msg "|| Cloning GCC ||"
+		git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 -b gcc-new gcc64
+		git clone --depth=1 https://github.com/mvaisakh/gcc-arm -b gcc-new gcc32
+		GCC64_DIR=$KERNEL_DIR/gcc64
+		GCC32_DIR=$KERNEL_DIR/gcc32
 
-	echo "★★GCC cloning done"
-	echo ""
-	git clone --depth 1 --no-single-branch https://github.com/archie9211/AnyKernel2 anykernel
-	echo "★★Cloning Kinda Done..!!!"
+	msg "|| Cloning Anykernel ||"
+	git clone --depth 1 --no-single-branch https://github.com/Reinazhard/AnyKernel3.git -b sadtoni
 }
 
 ##------------------------------------------------------##
 
-function exports {
-	export KBUILD_BUILD_USER="archie"
-	export KBUILD_BUILD_HOST="HyperBeast"
+exports() {
+	export KBUILD_BUILD_USER="reina"
+	export KBUILD_BUILD_HOST="Laptop-Sangar"
 	export ARCH=arm64
 	export SUBARCH=arm64
-	export CROSS_COMPILE_ARM32=$KERNEL_DIR/linaro32/bin/armv8l-linux-gnueabihf-
-	export CROSS_COMPILE=$KERNEL_DIR/linaro/bin/aarch64-linux-gnu-
+
+	KBUILD_COMPILER_STRING=$("$GCC64_DIR"/bin/aarch64-elf-gcc --version | head -n 1)
+	PATH=$GCC64_DIR/bin/:$GCC32_DIR/bin/:/usr/bin:$PATH
+
+	export CROSS_COMPILE_ARM32=$GCC32_DIR/bin/arm-eabi-
+	export CROSS_COMPILE=$GCC64_DIR/bin/aarch64-elf-
+	export PATH KBUILD_COMPILER_STRING
 	export BOT_MSG_URL="https://api.telegram.org/bot$token/sendMessage"
 	export BOT_BUILD_URL="https://api.telegram.org/bot$token/sendDocument"
-	export PROCS=$(nproc --all)
+	PROCS=$(nproc --all)
+	export PROCS
 }
 
 ##---------------------------------------------------------##
 
-function tg_post_msg {
-	curl -s -X POST "$BOT_MSG_URL" -d chat_id="$2" \
+tg_post_msg() {
+	curl -s -X POST "$BOT_MSG_URL" -d chat_id="-1001403511595" \
 	-d "disable_web_page_preview=true" \
 	-d "parse_mode=html" \
 	-d text="$1"
@@ -72,53 +125,65 @@ function tg_post_msg {
 
 ##----------------------------------------------------------------##
 
-function tg_post_build {
-	curl --progress-bar -F document=@"$1" $BOT_BUILD_URL \
+tg_post_build() {
+	#Post MD5Checksum alongwith for easeness
+	MD5CHECK=$(md5sum "$1" | cut -d' ' -f1)
+
+	#Show the Checksum alongwith caption
+	curl --progress-bar -F document=@"$1" "$BOT_BUILD_URL" \
 	-F chat_id="$2"  \
 	-F "disable_web_page_preview=true" \
 	-F "parse_mode=html" \
-	-F caption="$3"  
+	-F caption="$3 | <code>Build Number : </code><b>$DRONE_BUILD_NUMBER</b>"
 }
 
 ##----------------------------------------------------------##
 
-
-##----------------------------------------------------------##
-
-function build_kernel {
-	if [ "$build_push" = true ]; then
-		tg_post_msg "<b>$CIRCLE_BUILD_NUM CI Build Triggered</b>%0A<b>Date : </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Device : </b><code>$DEVICE</code>%0A<b>Pipeline Host : </b><code>CircleCI</code>%0A<b>Host Core Count : </b><code>$PROCS</code>%0A<b>Compiler Used : %0A Branch : </b><code>$CIRCLE_BRANCH</code>%0A<b>Status : </b>#Nightly" "$CHATID"
+build_kernel() {
+	if [ "$PTTG" = 1 ]
+ 	then
+		tg_post_msg "<b>🔨 $KBUILD_BUILD_VERSION CI Build Triggered</b>%0A<b>Kernel Version : </b><code>$KERVER</code>%0A<b>Date : </b><code>$(TZ=Asia/Jakarta date)</code>%0A<b>Compiler Used : </b><code>$KBUILD_COMPILER_STRING</code>%0a<b>Branch : </b><code>$CI_BRANCH</code>%0A<b>Top Commit : </b><a href='$DRONE_COMMIT_LINK'>$COMMIT_HEAD</a>" "$CHATID"
 	fi
-	make O=out $DEFCONFIG
+
+	msg "|| Started Compilation ||"
 	BUILD_START=$(date +"%s")
-	make -j$PROCS O=out \
-		CROSS_COMPILE=$CROSS_COMPILE \
-		CROSS_COMPILE_ARM32=$CROSS_COMPILE_ARM32 2>&1 | tee error.log
-	BUILD_END=$(date +"%s")
-	DIFF=$((BUILD_END - BUILD_START))
-	check_img
-}
+	make O=out $DEFCONFIG
+	make -j"$PROCS" O=out ARCH=arm64 LD=ld.lld
 
-##-------------------------------------------------------------##
+		BUILD_END=$(date +"%s")
+		DIFF=$((BUILD_END - BUILD_START))
 
-function check_img {
-	if [ -f $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb ] 
+		if [ -f "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb ]
 	    then
-		gen_zip
-	else
-		tg_post_build "error.log" "$CHATID" "<b>Build failed to compile after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</b>"
-	fi
+	    	msg "|| Kernel successfully compiled ||"
+				gen_zip
+		else
+			if [ "$PTTG" = 1 ]
+ 			then
+				tg_post_msg "<b>❌ Build failed to compile after $((DIFF / 60)) minute(s) and $((DIFF % 60)) seconds</b>" "$CHATID"
+			fi
+		fi
+
 }
 
 ##--------------------------------------------------------------##
 
-function gen_zip {
-	mv $KERNEL_DIR/out/arch/arm64/boot/Image.gz-dtb AnyKernel2/Image.gz-dtb
-# 	mv $KERNEL_DIR/out/arch/arm64/boot/dtbo.img AnyKernel2/dtbo.img
-	cd anykernel
-	zip -r9 $ZIPNAME-$DATE * -x .git README.md
-	MD5CHECK=$(md5sum $ZIPNAME-$DATE.zip | cut -d' ' -f1)
-	tg_post_build $ZIPNAME* "$CHATID" "Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s) | MD5 Checksum : <code>$MD5CHECK</code>"
+gen_zip() {
+	msg "|| Zipping into a flashable zip ||"
+	mv "$KERNEL_DIR"/out/arch/arm64/boot/Image.gz-dtb AnyKernel3/Image.gz-dtb
+	if [ $BUILD_DTBO = 1 ]
+	then
+		mv "$KERNEL_DIR"/out/arch/arm64/boot/dtbo.img AnyKernel3/dtbo.img
+	fi
+	cd AnyKernel3 || exit
+	zip -r9 $ZIPNAME-$DEVICE-"$DRONE_BUILD_NUMBER" * -x .git README.md
+
+	## Prepare a final zip variable
+	ZIP_FINAL="$ZIPNAME-$DEVICE-$DRONE_BUILD_NUMBER.zip"
+	if [ "$PTTG" = 1 ]
+ 	then
+		tg_post_build "$ZIP_FINAL" "$CHATID" "✅ Build took : $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s)"
+	fi
 	cd ..
 }
 
@@ -126,4 +191,4 @@ clone
 exports
 build_kernel
 
-##----------------*****-----------
+##----------------*****-----------------------------##
